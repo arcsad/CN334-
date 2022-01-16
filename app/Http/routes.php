40e5c7eae5601439ca5bf8cf077f960236a77415
arcsad <94112824+arcsad@users.noreply.github.com>
2022-01-16 22:51:04 +0700

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,7 +27,9 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/', function () {
         return view('welcome');
     })->middleware('guest');
-
+    Route::get('/user', function () {
+        echo 'oak';
+    });
     Route::get('/tasks', 'TaskController@index');
     Route::post('/task', 'TaskController@store');
     Route::delete('/task/{task}', 'TaskController@destroy');
