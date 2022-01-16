68f3bbc06b86a9d03d12769267b96b7f4dc6d67f
arcsad <94112824+arcsad@users.noreply.github.com>
2022-01-17 00:57:16 +0700

    change background color and change text welcome,changecolor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
+                <div class="panel-heading"><p style="color: 222222px;">Welcome to task list cn334</p> </div>
+                
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <img src="{{ asset('images.png') }}" width= '798' height='366' class="img-responsive center-block" />
                 </div>
             </div>
         </div>
