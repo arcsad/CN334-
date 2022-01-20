589f17b2e82f806a08af6bc3badacc4e81d5b38d
ThanchotD <Thanachot.wongmetin@gmail.com>
2022-01-20 14:02:28 +0700

    Update database

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading"><p style="color: 222222px;">INFINITY TASK</p> </div>
+                
+                <div class="panel-body">
+                    <img src="{{ asset('im.jpg') }}" width= '798*3' height='366*3' class="img-responsive center-block" />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
