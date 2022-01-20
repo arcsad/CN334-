e1a7b0eddb183d30d00b2c1b0437c7258b49b698
arcsad <94112824+arcsad@users.noreply.github.com>
2022-01-20 15:48:20 +0700

    make loading page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" id="page">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><p style="color: 222222px;">INFINITY TASK</p> </div>
-                
+                <div class="panel-heading"><p >INFINITY TASK</p> </div>
+
                 <div class="panel-body">
                     <img src="{{ asset('im.jpg') }}" width= '798*3' height='366*3' class="img-responsive center-block" />
 
@@ -15,4 +15,25 @@
         </div>
     </div>
 </div>
+<div id="loading"></div>
+<script>function onReady(callback) {
+        var intervalID = window.setInterval(checkReady, 1000);
+
+        function checkReady() {
+            if (document.getElementsByTagName('body')[0] !== undefined) {
+                window.clearInterval(intervalID);
+                callback.call(this);
+            }
+        }
+    }
+
+    function show(id, value) {
+        document.getElementById(id).style.display = value ? 'block' : 'none';
+    }
+
+    onReady(function () {
+        show('page', true);
+        show('loading', false);
+    });</script>
+
 @endsection
