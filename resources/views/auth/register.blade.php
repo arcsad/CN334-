7fac5b715f9ca8a8c270bb335317908cd490a35b
ThanchotD <Thanachot.wongmetin@gmail.com>
2022-01-21 21:33:58 +0700

    Code login page , welcome page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,31 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <title>INFINITY TASK</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&family=Roboto:wght@700&display=swap" rel="stylesheet">
+
+
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('app.css') }}" rel="stylesheet">
+
+</head>
+<body id="app-layout" >
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
-                <div class="panel-body login">
+
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
@@ -73,10 +92,19 @@
                                 </button>
                             </div>
                         </div>
+
                     </form>
-                </div>
+
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<!-- JavaScripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
+</html>
+
+
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,9 +6,13 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@200&family=Roboto:wght@700&display=swap" rel="stylesheet">
+
         <style>
             html, body {
                 height: 100%;
@@ -48,10 +52,10 @@
     <body>
         <div class="container">
             <div class="content">
-                <img src = "{{ asset('error.svg') }}" width= '500' height ='500' class="img-responsive center-block">
-                <h2>Oops!</h2>
+                <img src="{{ asset('error.svg') }}" width='500' height='500' class="img-responsive center-block">
+                <h1 style="font-weight: 900;">Oops!</h1>
                 <p>sorry ,no data found</p>
-               <div style="padding: 10px"> <button type="button" class="btn btn-danger" style="border: 1px solid black;background-color: #434343;"><a href = "{{ url('/') }}" >กลับหน้าหลัก</a></button> </div>
+               <div style="padding: 10px"> <button type="button" class="btn btn-dark" ><a href = "{{ url('/') }}" >กลับหน้าหลัก</a></button> </div>
             </div>
         </div>
     </body>
